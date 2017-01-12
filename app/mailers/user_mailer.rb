c3cb5d52485a754d95ca360b80bb54e157cfc469
Eric Greene <ericjgreene@gmail.com>
2017-01-12 14:00:29 -0500

    mailer updates

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,9 @@
 class UserMailer < ApplicationMailer
+  default :from => "machinegreene@gmail.com"
+
+  def welcome_email(user)
+    @user = user
+    # @url = ‘http://google.com’
+    mail :to => "ericjgreene@gmail.com", :subject => "Eric Greene Trim Questionaire",
+  end
 end
